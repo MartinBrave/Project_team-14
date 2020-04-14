d768b6366e7e2043eae10c4dc0023b0b57f27291
h-hafezi <h.hafezi2000@gmail.com>
2020-04-15 02:47:02 +0430

    class commit

---
+++ src/main/java/Model/Boss.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Boss {
+    private boolean isThereBoss;
+    public boolean isThereBoss(){
+        return false;
+    }
+
+    public void setIsThereBoss (boolean state){
+
+    }
+}
