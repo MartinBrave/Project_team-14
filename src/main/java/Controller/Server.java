aad82c556388dc4fe5b98321fe5ce5e43d662cf8
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-04-19 15:05:43 +0430

    ControllerPackage-ServerClass

--- src/main/java/Controller/Server.java
+++ src/main/java/Controller/Server.java
@@ -2,7 +2,26 @@ package Controller;
 
 import Model.Person;
 
+import java.util.ArrayList;
+
 public class Server {
-    private Person loginUser;
-    private String answer;
+    private ArrayList<Person> loginUsers;
+    private LoginManager loginManager;
+    private BossManager bossManager;
+    private CustomerManager customerManager;
+    private SalesmanManager salesmanManager;
+    private PageManager pageManager;
+    private ProductPageManager productPageManager;
+    private ArrayList<Product> abstractCart;
+
+    public Server() {
+        loginUsers = new ArrayList<>();
+        loginManager = new LoginManager();
+        bossManager = new BossManager();
+        customerManager = new CustomerManager();
+        salesmanManager = new SalesmanManager();
+        pageManager = new PageManager();
+        productPageManager = new ProductPageManager();
+        abstractCart = new ArrayList<Product>();
+    }
 }
