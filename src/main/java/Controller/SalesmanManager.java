2337d573327a330ebd99d5519e7ac04e323b2e34
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-04-19 15:18:05 +0430

    class commit

---
+++ src/main/java/Controller/SalesmanManager.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class SalesmanManager {
+    private SalesManager salesManager;
+    private ProductManager productManager;
+
+    public SalesmanManager() {
+        salesManager = new SalesManager();
+        productManager = new ProductManager();
+    }
+}
